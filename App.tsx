221f09af2f7c76649d4498c4abf02582d1fac545
Đỗ Thành Đạt <panda.snkrhead273@gmail.com>
2021-12-06 21:29:48 +0700

    basic ui

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Home from './src/screens/Home';
 
 const App = () => {
